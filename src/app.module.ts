2064308285bef510701c91c6131e513877cfcd1c
Durmuş <demirtasdurmus@gmail.com>
2023-03-20 07:42:11 +0300

    WIRE UP mongodb with mongoose in a sync way

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VehicleModule } from './vehicle/vehicle.module';
 
 @Module({
-  imports: [VehicleModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017', {
+      dbName: 'vehicles',
+      authSource: 'admin',
+      socketTimeoutMS: 15000,
+      connectionFactory(connection) {
+        console.log(`Connected to ${connection.name} successfully`);
+        return connection;
+      },
+    }),
+    VehicleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

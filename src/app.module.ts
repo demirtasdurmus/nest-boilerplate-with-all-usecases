4c03066d5e4833c1c7f7a7442fe850715de26f42
Durmuş <demirtasdurmus@gmail.com>
2023-03-21 07:24:31 +0300

    SIMPLIFY app module to organize modular structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { VehicleModule } from './vehicle/vehicle.module';
 
 @Module({
@@ -18,9 +16,7 @@ import { VehicleModule } from './vehicle/vehicle.module';
     }),
     VehicleModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({ whitelist: true }),

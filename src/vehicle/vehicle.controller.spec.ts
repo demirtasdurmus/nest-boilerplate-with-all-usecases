4fddf6e829bfaf8742cc0e7b3e4ab962f6a49d0d
Durmuş <demirtasdurmus@gmail.com>
2023-03-27 02:47:10 +0300

    ADD comments and auto mock logic to vehicle.controller.spec.ts

--- src/vehicle/vehicle.controller.spec.ts
+++ src/vehicle/vehicle.controller.spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { VehicleController } from './vehicle.controller';
 import { VehicleService } from './vehicle.service';
 
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+const moduleMocker = new ModuleMocker(global);
+
 describe('VehicleController', () => {
   let controller: VehicleController;
 
@@ -9,10 +12,25 @@ describe('VehicleController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [VehicleController],
       providers: [VehicleService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        const results = ['test1', 'test2'];
+        if (token === VehicleService) {
+          return { findAll: jest.fn().mockResolvedValue(results) };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     controller = module.get<VehicleController>(VehicleController);
   });
+
+  // const result = ['test'];
+  // jest.spyOn(vehicleService, 'findAll').mockImplementation(() => result);
 
   it('should be defined', () => {
     expect(controller).toBeDefined();

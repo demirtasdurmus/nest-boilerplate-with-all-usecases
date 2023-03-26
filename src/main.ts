db0ee9caadc18d5ed8ec81dec06c0742c4279de1
Durmuş <demirtasdurmus@gmail.com>
2023-03-27 01:37:15 +0300

    ADD notes for lazy loading

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { INestApplication } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { LazyModuleLoader, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<INestApplication>(AppModule);
+
+  // TODO: Couldn't get exactly, check this later
+  // lazy loading
+  // const { LazyModule } = await import('./lib/lazy/lazy.module');
+  // const lazyModuleLoader = app.get(LazyModuleLoader);
+  // const moduleRef = await lazyModuleLoader.load(() => LazyModule);
+
   await app.listen(8000);
 }
 bootstrap();

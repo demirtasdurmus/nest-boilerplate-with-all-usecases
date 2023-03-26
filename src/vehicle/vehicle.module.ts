3f6807e88dc13804103975f30039a8468521c4eb
Durmuş <demirtasdurmus@gmail.com>
2023-03-27 01:14:37 +0300

    ADD note for resolving circular dependencies

--- src/vehicle/vehicle.module.ts
+++ src/vehicle/vehicle.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { VehicleService } from './vehicle.service';
 import { VehicleController } from './vehicle.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Vehicle, VehicleSchema } from './schemas/vehicle.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Vehicle.name, schema: VehicleSchema }])],
+  imports: [
+    // forwardRef(()=> CircularDependentModule), // solving issues with circular dependency
+    MongooseModule.forFeature([{ name: Vehicle.name, schema: VehicleSchema }]),
+  ],
   controllers: [VehicleController],
   providers: [VehicleService],
 })

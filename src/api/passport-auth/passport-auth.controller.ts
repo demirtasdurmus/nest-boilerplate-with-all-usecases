43cb4a61a4af5aa9f5f6eb39b4ffbb49ac2e03f5
Durmuş <demirtasdurmus@gmail.com>
2023-04-08 06:11:26 +0300

    FIX cookie expiry

--- src/api/passport-auth/passport-auth.controller.ts
+++ src/api/passport-auth/passport-auth.controller.ts
@@ -4,10 +4,15 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 import { PassportAuthService } from './passport-auth.service';
 import { JwtBearerAuthGuard } from './guards/jwt-bearer.guard';
 import { JwtCookieAuthGuard } from './guards/jwt-cookie.guard';
+import { ConfigService } from '@nestjs/config';
+import { IConfig } from '../../config/config.interface';
 
 @Controller('passport-auth')
 export class PassportAuthController {
-  constructor(private readonly authService: PassportAuthService) {}
+  constructor(
+    private readonly authService: PassportAuthService,
+    private readonly config: ConfigService<IConfig, true>,
+  ) {}
 
   @Post('login-bearer')
   @HttpCode(HttpStatus.OK)
@@ -27,11 +32,15 @@ export class PassportAuthController {
   @UseGuards(LocalAuthGuard)
   async loginCookie(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
     const token = await this.authService.createAccessToken(req.user);
+
+    const day = this.config.get('JWT_EXPIRES_IN', { infer: true }).split('').slice(0, -1).join('');
+    const expires = new Date(Date.now() + Number(day) * 24 * 60 * 60 * 1000);
+
     res.cookie('access_token', token, {
       httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
-      expires: new Date(Date.now() + 1 * 24 * 60 * 1000),
+      secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+      sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'strict',
+      expires,
     });
 
     return { message: 'success' };
